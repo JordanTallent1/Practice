c0af488d6943a2d3e1b84fd68c2278d30861fd9b
JordanTallent1 <40626384+JordanTallent1@users.noreply.github.com>
2019-09-22 01:38:21 -0400

    Update build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
 ## Complete the following steps to get Docker running locally
-echo"hello world"
 
 # Step 1:
 docker build --tag=capstone .
 
 # Step 2: 
-echo"$(docker image ls)"
+docker image ls
 
 # Step 3: 
 # docker run -it -p 8000:80 capstone
