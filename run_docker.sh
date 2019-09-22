78dda3149ba607ae724c5adc762403c99c815fd6
JordanTallent1 <40626384+JordanTallent1@users.noreply.github.com>
2019-09-22 01:07:33 -0400

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -3,10 +3,10 @@
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
-docker build --tag=project5 .
+docker build --tag=capstone .
 
 # Step 2: 
 docker image ls
 
 # Step 3: 
-docker run -it -p 8000:80 project5
+docker run -it -p 8000:80 capstone
