8f1ed66da166c54d11b984236b6d9757c9566857
JordanTallent1 <40626384+JordanTallent1@users.noreply.github.com>
2019-10-08 00:24:41 -0400

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,6 +8,7 @@ dockerpath=jtallent2019/capstone
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+echo $var_docker_password | docker login --username jtallent2019 --password-stdin
 kubectl run test3 --image=jtallent2019/capstone:trying_again --port=80 
 
 
